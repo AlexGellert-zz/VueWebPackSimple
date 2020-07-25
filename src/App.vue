c1d8a1e6dbec9ef3291f2d007ccae2d72d7dce92
Alex Gellert <alexgellert1@gmail.com>
2020-07-25 12:07:32 -0400

    simple vue typescript with webpack deploy

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<style scoped>
+@import "./style/main.css";
+</style>
+
+<template>
+  <div class="container-fluid">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component
+export default class App extends Vue {
+
+  constructor() {
+    super();
+  }
+
+}
+</script>
